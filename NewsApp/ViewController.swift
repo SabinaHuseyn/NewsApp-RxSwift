62fac780cea029a3ff556ef8c3174b88360c94f9
SabinaHuseyn <sabinah3377@gmail.com>
2021-09-24 00:47:33 +0400

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Sabina Huseynova on 24.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
